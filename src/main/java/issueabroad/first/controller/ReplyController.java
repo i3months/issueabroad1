1394f0816b2902362f51e6c619c6f099caa9cb0e
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2023-02-04 17:28:58 +0900

    Reply Logic (3)

--- src/main/java/issueabroad/first/controller/ReplyController.java
+++ src/main/java/issueabroad/first/controller/ReplyController.java
@@ -10,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,21 @@ public class ReplyController {
     public ResponseEntity<List<ScrapReplyDTO>> getListByScrapBoard(@PathVariable("sno") Long sno) {
 
         return new ResponseEntity<>(scrapReplyService.getList(sno), HttpStatus.OK);
+    }
+
+    @PostMapping("/scrap/register")
+    public ResponseEntity<Long> scrapRegister(@RequestBody ScrapReplyDTO scrapReplyDTO) {
+        Long srno = scrapReplyService.register(scrapReplyDTO);
+
+        return new ResponseEntity<>(srno, HttpStatus.OK);
+    }
+
+    @PostMapping("/user/register")
+    public ResponseEntity<Long> userRegister(@RequestBody UserReplyDTO userReplyDTO) {
+
+        Long urno = userReplyService.register(userReplyDTO);
+
+        return new ResponseEntity<>(urno, HttpStatus.OK);
     }
 
 

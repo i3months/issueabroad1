a5557d570f5aa2ed5fb2d96d71c3422d48504c7a
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2023-02-09 03:50:50 +0900

    Security Apply

--- src/main/java/issueabroad/first/config/SecurityConfig.java
+++ src/main/java/issueabroad/first/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package issueabroad.first.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     PasswordEncoder passwordEncoder() {
